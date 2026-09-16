bdc0f67fa8581b04b4f61d461c06359146aefda5
minji0000 <bluurmi@gmail.com>
2026-09-16 17:21:47 +0900

    chore: initialize worship-zone

---
+++ src/main/java/com/kari/worshipzone/WorshipZoneApplication.java
@@ -0,0 +1,13 @@
+package com.kari.worshipzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshipZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorshipZoneApplication.class, args);
+    }
+
+}
